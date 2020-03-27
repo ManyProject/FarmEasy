3ff4d1a64ea29fb8f69ef4935637a633f107782a
unknown <aniket15b@gmail.com>
2020-03-27 17:44:46 +0530

    Added authentication and middleware

--- app.py
+++ app.py
@@ -1,20 +1,37 @@
-from flask import Flask, render_template, request, redirect
+from flask import Flask, render_template, request, redirect, url_for, session
 import mysql.connector
 
-from authentication import authenticate 
+from authentication import login, register, authentication_check
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'super secret key'
 
 @app.route('/', methods=['GET', 'POST'])
 
 def index():
-    if(request.method == 'POST'):
-        
-    return render_template('category.html')
-
-def cart():
-    print('Done')
     return "Hello"
+
+@app.route('/login', methods=['GET', 'POST'])
+
+def auth():
+    if request.method == 'GET' :
+        return render_template('login.html')
+    if request.method == 'POST' :
+        return login(app)
+
+@app.route('/register', methods=['GET', 'POST'])
+
+def registration():
+    if request.method == 'GET' :
+        return render_template('register.html')
+    if request.method == 'POST' :
+        return register(app)
+
+@app.route('/logout', methods=['GET'])
+
+def logout():
+    session.pop('email', None)
+    return redirect(url_for('index'))
 
 if __name__ == '__main__':
     app.run(debug=True)
