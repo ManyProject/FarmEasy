3fac2480fa9a1258275315d020c2ebc188c45d5d
urvinb <urvibheda999@gmail.com>
2020-03-29 13:28:00 +0530

    product.html

--- app.py
+++ app.py
@@ -2,19 +2,24 @@ from flask import Flask, render_template, request, redirect
 import mysql.connector
 
 from authentication import authenticate 
+from producedetails import productdetail
 
 app = Flask(__name__)
 
 @app.route('/', methods=['GET', 'POST'])
 
 def index():
-    if(request.method == 'POST'):
-        
     return render_template('category.html')
 
-def cart():
-    print('Done')
-    return "Hello"
+@app.route('/category')
+
+def category():
+    return render_template('category.html')
+
+@app.route('/product/<pname>')
+
+def product(pname):
+    return productdetail(pname)
 
 if __name__ == '__main__':
     app.run(debug=True)
