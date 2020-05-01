16c6aeacbb2660c8dc2029e7311aec3502e3ba3a
urvinb <you@example.com>
2020-05-01 22:27:29 +0530

    service-worker changes

--- static/service-worker.js
+++ static/service-worker.js
@@ -14,12 +14,19 @@ self.addEventListener('install', function(event) {
   );
 });
 
-self.addEventListener('fetch', function(event) {
-  console.log(event.request.url);
- 
+// self.addEventListener('fetch', function(event) {
+//   console.log(event.request.url);
+//   event.respondWith(
+//     caches.match(event.request).then(function(response) {
+//       return response || fetch(event.request);
+//     })
+//   );
+//  });
+
+ self.addEventListener('fetch', function(event) {
   event.respondWith(
-    caches.match(event.request).then(function(response) {
-      return response || fetch(event.request);
+    fetch(event.request).catch(function() {
+      return caches.match(event.request);
     })
   );
- });
+});
