1b07a8941919653d3e7e357c527b35616b9be9b3
aniket15b <aniket15b@gmail.com>
2020-05-01 11:56:22 +0530

    Update dc_connection.py for heroku

--- db_connection.py
+++ db_connection.py
@@ -1,10 +1,13 @@
 import mysql.connector
+import os
 
 def connect():
     return mysql.connector.connect(
-        host = "localhost",
-        user = "root",
-        passwd = "",
-        database = "farmeasy",
-        port = "3306"
+        host = os.environ.get('MYSQL_HOST', "localhost"),
+        user = os.environ.get('MYSQL_USER', "root"),
+        passwd = os.environ.get('MYSQL_PWD', ""),
+        database = os.environ.get('MYSQL_DB', "farmeasy"),
+        port = os.environ.get('MYSQL_PORT', "3306")
     )
+
+
