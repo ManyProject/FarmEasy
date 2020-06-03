a582f983de63613954c72b321333a124f2d504d0
unknown <aniket15b@gmail.com>
2020-06-04 01:40:28 +0530

    Test

--- controller/authentication.py
+++ controller/authentication.py
@@ -117,6 +117,7 @@ def register(app):
                  VALUES(UUID(), %s, %s, %s, %s, %s, %s)"
         x = random.randint(0, len(address)-1)
         cur.execute(query, (name, email, phone, address[x], role, pw_hash, ))
+        connection.commit()
         uuid = cur.lastrowid
         print(uuid)
         if(role == 'Farmer'):
